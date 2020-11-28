385cae726ad92c79bb09c6eb10b3606a088a4c18
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-11-28 15:05:34 +0000

    Add src/main/scala/com/grey/BuildingsApp.scala

---
+++ src/main/scala/com/grey/BuildingsApp.scala
@@ -0,0 +1,43 @@
+package com.grey
+
+import com.grey.inspectors.InspectArguments
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+
+object BuildingsApp {
+
+  def main(args: Array[String]): Unit = {
+
+
+    // Arguments
+    if (args.length == 0) {
+      sys.error("The YAML of parameters is required.")
+    }
+
+
+    // Inspect
+    val inspectArguments = InspectArguments
+    val parameters: InspectArguments.Parameters = inspectArguments.inspectArguments(args)
+
+
+    // Limiting log data streams
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+
+    // Spark session instance
+    val spark = SparkSession.builder()
+      .appName("buildings").getOrCreate()
+
+
+    // Spark logs level
+    spark.sparkContext.setLogLevel("ERROR")
+
+
+    // Proceed
+    new DataSteps(spark).dataSteps(parameters = parameters)
+
+
+  }
+
+}
