3ad0024043265eb152c3c0561d0d37e1a1e10dcd
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-02-04 21:44:29 +0000

    Update src/main/scala/com/grey/queries/RelationalOperators.scala

--- src/main/scala/com/grey/queries/RelationalOperators.scala
+++ src/main/scala/com/grey/queries/RelationalOperators.scala
@@ -12,11 +12,12 @@ class RelationalOperators(spark: SparkSession) {
   /**
     * Operators: =, <> or !=, >, <, >=, <=
     *
-    * @param buildings: For illustrating equivalent Dataset calculations
+    * @param buildings: The Dataset[Row] of buildings
     */
   def relationalOperators(buildings: Dataset[Row]): Unit = {
 
-    println("\n\nRelational Operators")
+
+    println("\n\nRelational Operators\n")
 
 
     // logging
@@ -24,34 +25,39 @@ class RelationalOperators(spark: SparkSession) {
     logger.info("\n\nRelational Operators")
 
 
-    // Import implicits for
-    //    encoding (https://jaceklaskowski.gitbooks.io/mastering-apache-spark/spark-sql-Encoder.html)
-    //    implicit conversions, e.g., converting a RDD to a DataFrames.
-    //    access to the "$" notation.
+    /**
+      * Import implicits for
+      *   encoding (https://jaceklaskowski.gitbooks.io/mastering-apache-spark/spark-sql-Encoder.html)
+      *   implicit conversions, e.g., converting a RDD to a DataFrames.
+      *   access to the "$" notation.
+      */
     import spark.implicits._
 
 
-    // greater than
+    /**
+      * Example: greater than, >
+      */
     val west: DataFrame = spark.sql("SELECT * FROM buildings WHERE west > 50")
-
     val westSet: Long = buildings.filter($"west" > 50).count()
 
     println(s"The # of months, since 1968, during which more than 50 thousand housing units " +
       s"where delivered in a month\n sql: ${west.count()}, dataset: $westSet")
 
 
-    // less than
+    /**
+      * Example: less than, <
+      */
     val south: DataFrame = spark.sql("SELECT * FROM buildings WHERE south < 20")
-
     val southSet: Long = buildings.filter($"south" < 20).count()
 
     println(s"The # of months, since 1968, during which fewer than 20 thousand housing units " +
       s"where delivered in a month\n sql: ${south.count()}, dataset: $southSet")
 
 
-    // equal to
+    /**
+      * Example: equal to, = or ===
+      */
     val february: DataFrame = spark.sql("SELECT * FROM buildings WHERE month_name = 'February'")
-
     val februarySet: Long = buildings.filter($"month_name" === "February").count()
 
     println(s"The # of records, i.e., the # of months, since 1968, associated with the month of February\n" +
