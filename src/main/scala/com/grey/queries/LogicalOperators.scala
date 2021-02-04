ebf0e17aa5406042903e0100fafe5a566dc5b0c7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-02-04 21:44:15 +0000

    Update src/main/scala/com/grey/queries/LogicalOperators.scala

--- src/main/scala/com/grey/queries/LogicalOperators.scala
+++ src/main/scala/com/grey/queries/LogicalOperators.scala
@@ -11,9 +11,12 @@ import org.apache.spark.sql.{Dataset, Row, SparkSession}
 class LogicalOperators(spark: SparkSession) {
 
   /**
-    * Operators: LIKE, IN, BETWEEN, IS NULL, AND, OR, NOT
+    * Operators: like, in, between, is null, and, or, not
+    *
+    * @param buildings: The Dataset[Row] of buildings
     */
   def logicalOperators(buildings: Dataset[Row]): Unit = {
+
 
     println("\n\nLogical Operators")
 
@@ -23,45 +26,57 @@ class LogicalOperators(spark: SparkSession) {
     logger.info("\n\nLogical Operators")
 
 
-    // Import implicits for
-    //    encoding (https://jaceklaskowski.gitbooks.io/mastering-apache-spark/spark-sql-Encoder.html)
-    //    implicit conversions, e.g., converting a RDD to a DataFrames.
-    //    access to the "$" notation.
+    /**
+      * Import implicits for
+      *   encoding (https://jaceklaskowski.gitbooks.io/mastering-apache-spark/spark-sql-Encoder.html)
+      *   implicit conversions, e.g., converting a RDD to a DataFrames.
+      *   access to the "$" notation.
+      */
     import spark.implicits._
 
 
-    // LIKE & case sensitive problems: like is case sensitive
+    /**
+      * Example: like & case sensitive problems (like is case sensitive)
+      */
     val sensitive: Long = spark.sql("SELECT count(*) AS n FROM buildings WHERE month_name LIKE 'Ju%'")
       .head().getAs[Long]("n")
-
-    val sensitiveSet: Long = buildings.filter($"month_name".startsWith("Ju")).count()
+    val sensitiveSet: Long = buildings.filter($"month_name".like("Ju%")).count()
+    val sensitiveStartsWith: Long = buildings.filter($"month_name".startsWith("Ju")).count()
 
     println(s"The # of records, since 1968, wherein the month string starts with 'Ju' exactly" +
-      s"\n sql: $sensitive, dataset: $sensitiveSet")
+      s"\nsql: $sensitive, dataset: $sensitiveSet, $sensitiveStartsWith")
 
 
-    // LIKE & case insensitive problems
+    /**
+      * Example: like & case insensitive problems
+      */
     val insensitive: Long = spark.sql("SELECT * FROM buildings WHERE lower(month_name) LIKE 'ju%'").count()
-
-    val insensitiveSet: Long = buildings.filter(lower($"month_name").startsWith("ju")).count()
+    val insensitiveSet: Long = buildings.filter(lower($"month_name").like("ju%")).count()
+    val insensitiveStartsWith: Long = buildings.filter(lower($"month_name").startsWith("ju")).count()
 
     println(s"The # of records, since 1968, wherein the month string starts with 'ju', regardless of case" +
-      s"\n sql: $insensitive, dataset: $insensitiveSet")
+      s"\nsql: $insensitive, dataset: $insensitiveSet, $insensitiveStartsWith")
 
 
-    // IN
+    /**
+      * Example: in
+      */
     val listOf: Seq[String] = Seq("march", "june", "september", "december")
     val listOfString: String = listOf.mkString("'", "','", "'")
 
-    println(s"The # of records, since 1968, associated with the months 'march', 'june', 'september', & 'december'\n" +
-      "sql: " +
-      spark.sql(s"SELECT * FROM buildings WHERE lower(month_name) IN ($listOfString)").count() +
-      ", dataset: " +
-      buildings.filter(lower($"month_name").isInCollection(listOf)).count()
+    val isMember: Long = spark.sql(s"SELECT * " +
+      s"FROM buildings WHERE lower(month_name) IN ($listOfString)").count()
+    val isMemberSet: Long = buildings.filter(lower($"month_name")
+      .isInCollection(listOf)).count()
+
+    println(s"\nThe # of records, since 1968, associated with the months 'march', 'june', 'september', & 'december'\n" +
+      s"sql: $isMember, dataset: $isMemberSet"
     )
 
 
-    // BETWEEN
+    /**
+      * Example: between
+      */
     println(s"\nThe # of records whereby the month value is between 1 & 3, i.e., is 1, 2, or 3\n" +
       "sql: " +
       spark.sql("SELECT * FROM buildings WHERE month >= 1 AND month <= 3").count() +
@@ -70,8 +85,10 @@ class LogicalOperators(spark: SparkSession) {
     )
 
 
-    // IS NULL
-    println("The number of cases whereby the month name is null\n" +
+    /**
+      * Example: is null
+      */
+    println("\nThe number of cases wherein the month name is null\n" +
       "sql: " +
       spark.sql("SELECT * FROM buildings WHERE month_name IS NULL").count() +
       ", dataset: " +
