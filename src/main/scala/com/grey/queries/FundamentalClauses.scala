a0321ec85cf0f1e36a25e1c8483da80ce99bbfc3
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-02-04 21:45:01 +0000

    Update src/main/scala/com/grey/queries/FundamentalClauses.scala

--- src/main/scala/com/grey/queries/FundamentalClauses.scala
+++ src/main/scala/com/grey/queries/FundamentalClauses.scala
@@ -17,22 +17,49 @@ class FundamentalClauses(spark: SparkSession) {
   def fundamentalClauses(buildings: Dataset[Row]): Unit = {
 
 
-    println("\n\nFundamental Clauses")
+    println("\n\nFundamental Clauses\n")
 
 
     // logging
     val logger = Logger(classOf[FundamentalClauses])
     logger.info("\n\nFundamental Clauses")
 
-    // select
-    spark.sql("SELECT year, month, west FROM buildings").show(5)
 
-    // limit
-    spark.sql("SELECT * FROM buildings LIMIT 5").show()
+    /**
+      * Import implicits for
+      *   encoding (https://jaceklaskowski.gitbooks.io/mastering-apache-spark/spark-sql-Encoder.html)
+      *   implicit conversions, e.g., converting a RDD to a DataFrames.
+      *   access to the "$" notation.
+      */
+    import spark.implicits._
 
-    // order by
-    spark.sql("SELECT date, year, month, month_name, midwest FROM buildings " +
-      "ORDER BY year").show(5)
+
+    /**
+      * Example: limit()
+      */
+    val limit: Int = 5
+
+    println("\n\nA preview of fields year, month, and west w.r.t. sql & dataset, respectively")
+    spark.sql(s"SELECT year, month, west FROM buildings LIMIT $limit").show()
+    buildings.select($"year", $"month", $"west").show(limit)
+
+    println("\n\nA preview of all fields w.r.t. sql & dataset, respectively")
+    spark.sql(s"SELECT * FROM buildings LIMIT $limit").show()
+    buildings.show(limit)
+
+
+    /**
+      * Example: order by
+      */
+    println("\n\nA preview of the fields year, date, month_name, and midwest wherein the " +
+      "data is ordered by ascending midwest value, and ascending date; nulls last in each case.  " +
+      "The previews are w.r.t. sql & dataset, respectively")
+
+    spark.sql("SELECT year, date, month_name, midwest FROM buildings " +
+      "ORDER BY midwest ASC NULLS LAST, date ASC NULLS LAST").show(5)
+
+    buildings.select($"year", $"date", $"month_name", $"midwest")
+      .orderBy($"midwest".asc_nulls_last, $"date".asc_nulls_last)
 
   }
 
